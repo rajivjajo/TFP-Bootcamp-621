7dfd9a77edc7407239469ad0126f16d0b96078eb
rajivjajo <rajivjajodia82552@gmail.com>
2022-11-26 17:52:46 +0530

    first commit for repo

---
+++ TwoDigitRandomnumber.sh
@@ -0,0 +1,9 @@
+#Task1->Generate Two Digit Random Number.
+
+
+#!/bin/bash
+
+num1=$((RANDOM%89+10)); 
+echo $num1;
+
+
---
+++ threeDigitrandom.sh
@@ -0,0 +1,7 @@
+#three Digit No:-
+<<<<<<< HEAD
+num3=$((RANDOM%899+100));
+=======
+num3=$((RANDOM%899+100)); 
+>>>>>>> day5
+echo $num3;
